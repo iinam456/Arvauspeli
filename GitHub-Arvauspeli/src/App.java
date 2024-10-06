825377a94b566e5672a7b8f7a5a6bb0aff9fc0fc
iinam456 <iina.helin@student.hamk.fi>
2024-10-06 14:59:05 +0300

    Lisätty if-metodi

--- GitHub-Arvauspeli/src/App.java
+++ GitHub-Arvauspeli/src/App.java
@@ -5,9 +5,14 @@ public class App {
 
         Scanner in = new Scanner(System.in);
         String input;
+        String rightName = "Sara";
 
         System.out.println("Guess my name!");
         input = in.nextLine();
+
+        if (input.equals("Sara")) {
+            System.out.println("You guessed right!");
+        }
 
     }
 }
