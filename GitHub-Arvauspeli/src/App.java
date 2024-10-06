c4e794c959c6ab5069aee8dbfca0476b7acf6ed7
iinam456 <iina.helin@student.hamk.fi>
2024-10-06 14:56:34 +0300

    Tehtävä aloitettu

---
+++ GitHub-Arvauspeli/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        System.out.println("Guess my name!");
+        input = in.nextLine();
+
+    }
+}
